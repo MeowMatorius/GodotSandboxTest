03b63c1bdccd3cf1aadfad5cecac483fea30b850
VIV2020 <none@idi.naxuy>
2025-10-19 03:02:23 +0300

    поправила dash

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 120.0
-const boost = 3
+const boost = 2
 const JUMP_VELOCITY = -300.0
 const DOUBLE_JUMP_VELOCITY = -100.0
 var count_jump = 1
 var dash = false
 var direction
+var dash_is_processing = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var timer: Timer = $Timer
@@ -29,21 +30,6 @@ func _physics_process(delta: float) -> void:
 
 	# Получение кнопок движения и управление ускорением/замедлением
 	direction = Input.get_axis("move_left", "move_right")
-	if Input.is_action_just_pressed("dash") and is_on_floor():
-		velocity.x = direction * SPEED * boost
-		dash = true
-		timer.start()
-	elif direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	# Рывок
-	#if Input.is_action_just_pressed("dash") and is_on_floor():
-		#velocity.x = direction * SPEED * boost
-		#dash = true
-		#timer.start()
-		
 
 	move_and_slide()
 
@@ -57,15 +43,24 @@ func _physics_process(delta: float) -> void:
 	
 	# Анимации игрока
 	if is_on_floor():
-		if direction == 0:
+		if direction == 0 and dash == false:
 			animated_sprite.play("idle")
-		elif dash == true and direction != 0:
-			animated_sprite.play("dash")
-		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		elif Input.is_action_just_pressed("dash") and direction != 0:
+			animated_sprite.play("dash_new")
+			velocity.x = direction * SPEED * boost
+			dash = true
+		elif dash == false and direction:
 			animated_sprite.play("run")
-	else:
+			velocity.x = direction * SPEED
+	elif dash == false:
 		animated_sprite.play("jump")
 
 
-func _on_timer_timeout() -> void:
-	dash = false
+#func _on_timer_timeout() -> void:
+	#dash = false
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if (animated_sprite.animation == "dash_new"):
+		dash = false
