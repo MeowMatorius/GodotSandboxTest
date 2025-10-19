0d704f6e015f0ec4bdb80ce4a5e48b34f26e44c1
MeowMatorious <No email>
2025-10-19 12:58:41 +0300

    Дополнил UI / Пофиксил флаг килзоны / Слаймы двигаются / Пауза добавлена

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var score = 0
+var paused = false
+
+@onready var coins: Label = %PlayerUI/PlayerStatus/ValuesContainer/Coins
+@onready var pause_menu: CanvasLayer = %PauseMenu
+
+# Увеличение счетчика монет
+func add_point():
+	score += 1
+	print(score)
+	coins.text = "Coins: " + str(score)
+
+
+func _process(delta: float) -> void:
+	
+	# Кнопка Паузы
+	if Input.is_action_just_pressed("menu"):
+		pauseMenu()
+	
+	# Кнопка Магазина
+	if Input.is_action_just_pressed("shop"):
+		pass
+
+
+# Меню Паузы
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+		paused = false
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+		paused = true
