2a4684fc2622afd92101d7005b91daf3de1fd40d
MeowMatorious <No email>
2025-10-19 15:25:01 +0300

    UI и GameManager теперь глобальные

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
-@onready var game_manager: Node = %GameManager
+@onready var game_manager = GameManager
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_point()
--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,18 +3,13 @@ extends Node
 var score = 0
 var paused = false
 
-@onready var coins: Label = %PlayerUI/PlayerStatus/ValuesContainer/Coins
-@onready var pause_menu: CanvasLayer = %PauseMenu
+@onready var coins: Label = $/root/PlayerUi/PlayerStatus/ValuesContainer/Coins
+@onready var pause_menu = PauseMenu
 
-# Увеличение счетчика монет
-func add_point():
-	score += 1
-	print(score)
-	coins.text = "Coins: " + str(score)
-
+func _ready() -> void:
+	pause_menu.hide()
 
 func _process(_delta: float) -> void:
-	
 	# Кнопка Паузы
 	if Input.is_action_just_pressed("menu"):
 		pauseMenu()
@@ -34,3 +29,10 @@ func pauseMenu():
 		pause_menu.show()
 		Engine.time_scale = 0
 		paused = true
+
+
+# Увеличение счетчика монет
+func add_point():
+	score += 1
+	print(score)
+	coins.text = "Coins: " + str(score)
