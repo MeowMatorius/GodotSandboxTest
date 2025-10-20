968bf9df5bda69d52fd654703c063b8fbcb58d2e
MeowMatorious <No email>
2025-10-20 17:41:54 +0300

    Статы / перестановка файлов

--- MenuComponents/Scripts/pause_menu.gd
+++ MenuComponents/Scripts/pause_menu.gd
@@ -2,14 +2,18 @@ extends CanvasLayer
 
 @onready var pause_menu = PauseMenu
 @onready var game_manager = GameManager
+@onready var stats_manager = StatsManager
+
 
 var paused = false
-var coin_amount = 10
-var exp_amount = 10
+var coin_amount = 50
+var exp_amount = 50
 var pauseTime = 0
+
 
 func _ready() -> void:
 	pause_menu.hide()
+
 
 func _process(_delta: float) -> void:
 	# Кнопка Паузы
@@ -27,32 +31,30 @@ func pauseMenu():
 		resume()
 	else:
 		pause()
+
 func resume():
 	pause_menu.hide()
 	Engine.time_scale = 1
 	paused = false
+
 func pause():
 	pause_menu.show()
 	Engine.time_scale = pauseTime
 	paused = true
 
 
-# Логика кнопок
+# Логика кнопок Main Menu
 func _on_resume_button_up() -> void:
 	resume()
-
 
 func _on_exit_button_up() -> void:
 	get_tree().quit()
 
-
 func _on_give_coins_button_up() -> void:
 	game_manager.add_coin(coin_amount)
 
-
 func _on_give_exp_button_up() -> void:
-	game_manager.add_exp(exp_amount)
-
+	stats_manager.add_exp(exp_amount)
 
 func _on_damage_player_button_up() -> void:
 	pass # Replace with function body.
---
+++ MenuComponents/Scripts/tab_bar.gd
@@ -0,0 +1,17 @@
+extends TabBar
+
+var currentTab
+var allTabs = []
+
+func _ready() -> void:
+	hide_tabs()
+	get_child(0).show()
+
+func _on_tab_clicked(tab: int) -> void:
+	currentTab = tab
+	hide_tabs()
+	get_child(currentTab).show()
+
+func hide_tabs():
+	for child in get_children(false):
+		child.hide()
