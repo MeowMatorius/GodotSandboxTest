6a8431c2535f12b76b9995fcef71c03b08d919b5
MeowMatorious <No email>
2025-10-23 23:23:29 +0300

    Реорганизация файлов

---
+++ Custom/Managers/game_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var location_1: String = "Sandbox"
+var location_2: String = "Snow"
+
+func _ready() -> void:
+	PauseMenu.exit_pause_menu()
+
+
+func player_reset(hard_reset: bool):
+	if hard_reset:
+		InventoryManager.reset_collectibles()
+		HealthManager.reset_health()
+		StatsManager.reset_player_stats()
+	else:
+		HealthManager.reset_health()
+
+
+func change_scene(string):
+	match string:
+		location_1:
+			get_tree().change_scene_to_file("res://Levels/level_1.tscn")
+		location_2:
+			get_tree().change_scene_to_file("res://Levels/level_2.tscn")
+	PauseMenu.exit_pause_menu()
